949bc21accd95c6e458720d3793d046c4d8eccdc
Pann Phyu <pann.phyu@kis-mm.com>
2025-01-04 23:38:13 +0630

    adding new tables

--- bank_management_system/models.py
+++ bank_management_system/models.py
@@ -17,12 +17,25 @@ class Account(Base):
     account_type = Column(String)
     balance = Column(Float, default=0.0)
 
+class Loan(Base):
+    __tablename__ = 'loan'
+    id = Column(Integer, primary_key=True)
+    loan_id = Column(Integer, ForeignKey('customers.id'))
+    loan_type = Column(String)
+    loan_amount = Column(Float)
+
 class Transaction(Base):
     __tablename__ = 'transactions'
     id = Column(Integer, primary_key=True)
     account_id = Column(Integer, ForeignKey('accounts.id'))
     amount = Column(Float)
     transaction_type = Column(String)
+
+class Branch(Base):
+    __tablename__ = 'branch'
+    id = Column(Integer, primary_key=True)
+    branch_name = Column(String)
+    branch_address = Column(String)
 
 def create_tables(engine):
     Base.metadata.create_all(engine)
